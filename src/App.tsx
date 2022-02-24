174de402a9cfb46c4a7bfdb22d2d96aea2c71847
Helen Hagos <hahagos@umich.edu>
2022-02-23 21:54:54 -0500

    RHEI/HHAG - Added a categories page with hardcoded fruit names

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Categories from './Categories';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Hi Helen!
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <Categories/>
     );
 }
 
