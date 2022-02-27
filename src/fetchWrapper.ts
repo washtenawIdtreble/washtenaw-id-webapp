c8343e5600bcb9e765844a42fcf015aa6f35188c
Helen Hagos <hahagos@umich.edu>
2022-02-27 12:59:40 -0500

    RHEI/HHAG - Created a wrapper for fetch. Added polyfill for fetch. Made the useCategoires hook fetch categories from an API.

---
+++ src/fetchWrapper.ts
@@ -0,0 +1,3 @@
+export default function fetchWrapper(input?: Request | string, init?: RequestInit): Promise<Response>{
+   return fetch(input, init);
+}
--- src/hooks/useCategories.test.tsx
+++ src/hooks/useCategories.test.tsx
@@ -1,11 +1,44 @@
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import useCategories from "./useCategories";
+import fetchWrapper from "../fetchWrapper";
 
-
+jest.mock("../fetchWrapper");
 describe(useCategories.name, ()=> {
-    test("should return a list of categories", () => {
-        render(<StubComponent></StubComponent>);
-        expect(categories).toEqual(["cranberries", "banana", "cherry"]);
+    let expectedData: string[];
+    let releaseTheHounds: () => void;
+    let rerender : any;
+    beforeEach(() => {
+        expectedData = ["cranberries", "banana", "cherry"];
+        const mockResponseJson = jest.fn().mockName("response.json");
+        mockResponseJson.mockResolvedValue(expectedData);
+        
+        jest.mocked(fetchWrapper)
+        .mockName("fetch")
+        .mockImplementation(() => {
+            return new Promise((resolve, reject) => {
+                releaseTheHounds = () => {
+                    //@ts-ignore
+                    resolve({
+                        json: mockResponseJson
+                    })
+                }
+            });
+        });
+        rerender = render(<StubComponent></StubComponent>).rerender;
+    });
+    test("should return an empty list of categories before fetch is complete", () => {
+        expect(categories).toEqual([]);
+    });
+    test("should make a request to get the categories", () => {
+        expect(fetchWrapper).toHaveBeenCalledWith("categories");
+    });
+    test("should update the categories with the response from fetch", async () => {
+        await waitFor(() => releaseTheHounds());
+        expect(categories).toBe(expectedData);
+    });
+    test("should call fetch only once", () => {
+        rerender(<StubComponent></StubComponent>);
+        expect(fetchWrapper).toHaveBeenCalledTimes(1);
     });
 });
 
