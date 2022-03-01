122ea0d8fb37f5c3ad90b952cb4645cafd9f6124
Ryan Heisler <info@ryanheisler.com>
2022-03-01 11:01:41 -0500

    RHEI - Quiet console warnings from MSW that appeared when the app hot reloaded

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,22 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { mockWorker } from "./mock-server/mock-worker";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { mockWorker, onUnhandledRequest } from "./mock-server/mock-worker";
+import { StartOptions } from "msw";
 
-if (process.env.NODE_ENV === 'development') {
-    mockWorker.start().catch(e => console.error("Mock Worker failed to start:", e));
+if (process.env.NODE_ENV === "development") {
+    let options: StartOptions = {
+        onUnhandledRequest: onUnhandledRequest
+    };
+    mockWorker.start(options).catch(e => console.error("Mock Worker failed to start:", e));
 }
 
 ReactDOM.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root"),
 );
--- src/mock-server/mock-worker.ts
+++ src/mock-server/mock-worker.ts
@@ -1,7 +1,17 @@
 import { setupWorker } from "msw";
 import { handlers } from "./production-server-handlers";
+import { UnhandledRequestStrategy } from "msw/lib/types/utils/request/onUnhandledRequest";
 
 /**
- * This is used as the mock server when the app is running
+ * This is used as the mock server when the app is running locally from source
  */
 export const mockWorker = setupWorker(...handlers);
+
+export const onUnhandledRequest: UnhandledRequestStrategy = ({ method, url }) => {
+    const ignoredSubstrings = [
+        "hot-update", // "hot-update" appears in requests made when the app hot reloads when running from source
+    ];
+    if (!ignoredSubstrings.some(substring => url.pathname.includes(substring))) {
+        throw new Error(`Unhandled ${method} request to ${url}`);
+    }
+};
