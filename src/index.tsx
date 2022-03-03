c03cf9c06ca9a30b17b7754b3a1f44ef8f0490d1
Ryan Heisler <info@ryanheisler.com>
2022-03-02 22:45:41 -0500

    RHEI - Retrieve server base URL based on an environment variable. Add scripts to run the react app and look for data from a server on localhost or the MSW fake server. Exclude mock server from bundle and don't import it in production to avoid missing dependency issue on Heroku

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { mockWorker, onUnhandledRequest } from "./mock-server/mock-worker";
 import { StartOptions } from "msw";
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development" && process.env.REACT_APP_API === "fake-data") {
+    const { mockWorker, onUnhandledRequest } = require("./mock-server/mock-worker");
     let options: StartOptions = {
-        onUnhandledRequest: onUnhandledRequest
+        onUnhandledRequest: onUnhandledRequest,
     };
-    mockWorker.start(options).catch(e => console.error("Mock Worker failed to start:", e));
+    mockWorker.start(options).catch((e: any) => console.error("Mock Worker failed to start:", e));
 }
 
 ReactDOM.render(
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,6 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import 'jest-when';
-import './mock-server/setup-server';
+import "@testing-library/jest-dom";
+import "jest-when";
+import "./mock-server/setup-server";
+
+process.env.REACT_APP_API = "fake-data";
