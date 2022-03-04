8acbe023a427664ab8a9fd27f1e5a0e391a7f593
Ryan Heisler <info@ryanheisler.com>
2022-03-04 13:14:54 -0500

    RHEI - Remove msw import from production code. Added an ESLint rule to show an error if production code is trying to import from msw.

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { StartOptions } from "msw";
 
 if (process.env.NODE_ENV === "development" && process.env.REACT_APP_API === "fake-data") {
     const { mockWorker, onUnhandledRequest } = require("./mock-server/mock-worker");
-    let options: StartOptions = {
-        onUnhandledRequest: onUnhandledRequest,
-    };
-    mockWorker.start(options).catch((e: any) => console.error("Mock Worker failed to start:", e));
+
+    mockWorker.start({ onUnhandledRequest: onUnhandledRequest })
+        .catch((e: any) => console.error("Mock Worker failed to start:", e));
 }
 
 ReactDOM.render(
--- src/mock-server/test-server-handlers.ts
+++ src/mock-server/test-server-handlers.ts
@@ -1,7 +1,7 @@
-import { DefaultRequestMultipartBody, PathParams, ResponseResolver, rest, RestContext, RestRequest } from "msw";
+import { DefaultRequestMultipartBody, ResponseResolver, rest, RestContext, RestRequest } from "msw";
 import { customCategoriesResolver, TEST_CATEGORIES } from "./categories-resolver";
 
-export type RequestResolver = ResponseResolver<RestRequest<never, PathParams>, RestContext, Record<string, any> | DefaultRequestMultipartBody | string | number | boolean | null | undefined>
+export type RequestResolver = ResponseResolver<RestRequest<never>, RestContext, Record<string, any> | DefaultRequestMultipartBody | string | number | boolean | null | undefined>
 
 export const handlers = [
     rest.get("/categories", customCategoriesResolver(TEST_CATEGORIES)),
