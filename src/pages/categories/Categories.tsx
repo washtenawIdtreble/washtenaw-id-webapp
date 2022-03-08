bc70007407ca7657ce0a09b01ebf9c8a9dcedc69
Ryan Heisler <info@ryanheisler.com>
2022-03-08 17:07:52 -0500

    RHEI - Use classes instead of adding style to ul and li elements directly to avoid multiple components squashing each others' styles

--- src/pages/categories/Categories.tsx
+++ src/pages/categories/Categories.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import useCategories from "../../hooks/useCategories";
-import "./Categories.css";
 import "../../utilities/to-title-case";
 import { toTitleCase } from "../../utilities/to-title-case";
+import "./Categories.css";
 
 export default function Categories() {
     const categories = useCategories();
 
     return (
-        <ul data-testid="categories-page">
-            {categories.map(category => <li key={category}>{toTitleCase(category)}</li>)}
+        <ul className={"categories-list"} data-testid="categories-page">
+            {categories.map(category =>
+                <li
+                    className={"category-item"}
+                    key={category}>
+                    {toTitleCase(category)}
+                </li>)}
         </ul>
     );
 }
