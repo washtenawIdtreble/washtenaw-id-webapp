644bc8a31c1e4028387a1536db81a9bbb37bfd04
Ryan Heisler <info@ryanheisler.com>
2022-10-26 21:07:20 -0400

    Treble 39 - Add validation function that can check if a phone number is invalid

---
+++ src/hooks/form-validation/validatePhone.test.ts
@@ -0,0 +1,36 @@
+import { INVALID_PHONE_MESSAGE, validatePhone } from "./validatePhone";
+
+describe(validatePhone.name, () => {
+    test("default error message is correct", () => {
+        expect(INVALID_PHONE_MESSAGE).toEqual("This is not a valid phone number. Make sure it contains 7 or 10 digits excluding a 1 at the beginning");
+    });
+    const validPhoneTestCases = [
+        { phone: "4642145", testCondition: "7 digits" },
+        { phone: "464-2145", testCondition: "hyphenated" },
+        { phone: "464 2145", testCondition: "spaces" },
+        { phone: "464.2145", testCondition: "periods" },
+        { phone: "(810) 464-2145", testCondition: "10 digits" },
+        { phone: "+1 (810) 464-2145", testCondition: "10 digits leading +1" },
+        {
+            phone: "!@#$%^&*()~-=_8104^&*()~-=_6421+[]{};':\",./<>/45",
+            testCondition: "containing other non alpha-numeric characters",
+        },
+    ];
+    validPhoneTestCases.forEach(testCase => {
+        test(`returns empty string for a valid phone - ${testCase.testCondition}`, () => {
+            expect(validatePhone(testCase.phone)).toEqual("");
+        });
+    });
+    const invalidPhoneTestCases = [
+        { phone: "464-Gb-2145", testCondition: "containing any letters" },
+        { phone: "464-214", testCondition: "with less than 7 digits" },
+        { phone: "464-2145-9", testCondition: "with between 7 and 10 digits" },
+        { phone: "(810) 464-2145-9", testCondition: "with more than 10 digits" },
+    ];
+    invalidPhoneTestCases.forEach(testCase => {
+        test(`returns error message for phone ${testCase.testCondition}`, () => {
+            const errorMessage = validatePhone(testCase.phone);
+            expect(errorMessage).toEqual(INVALID_PHONE_MESSAGE);
+        });
+    });
+});
---
+++ src/hooks/form-validation/validatePhone.ts
@@ -0,0 +1,18 @@
+import { Validator } from "./useValidations";
+
+export const INVALID_PHONE_MESSAGE = "This is not a valid phone number. Make sure it contains 7 or 10 digits excluding a 1 at the beginning";
+
+export const validatePhone: Validator = (phoneNumber: string) => {
+    const alphanumericPhone = phoneNumber.replace(/[^a-zA-Z\d]/g, "");
+    const digitsOnly = alphanumericPhone.replace(/[a-z]/g, "");
+    const withoutLeadingOne = digitsOnly.startsWith("1") ? digitsOnly.substring(1) : digitsOnly;
+
+    const invalid = (withoutLeadingOne.length !== 10 && withoutLeadingOne.length !== 7)
+        || digitsOnly.length < alphanumericPhone.length;
+
+    if (invalid) {
+        return INVALID_PHONE_MESSAGE;
+    }
+
+    return "";
+};
