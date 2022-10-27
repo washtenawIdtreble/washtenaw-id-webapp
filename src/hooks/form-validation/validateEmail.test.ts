da7e7732b9a1e8d55185b62f7015eeaece1d4555
Ryan Heisler <info@ryanheisler.com>
2022-10-26 20:25:08 -0400

    Treble 39 - Add validation function that can check if an email is invalid

---
+++ src/hooks/form-validation/validateEmail.test.ts
@@ -0,0 +1,26 @@
+import { INVALID_EMAIL_MESSAGE, validateEmail } from "./validateEmail";
+
+describe(validateEmail.name, () => {
+    test("default error message is correct", () => {
+        expect(INVALID_EMAIL_MESSAGE).toEqual("This is not a valid email address. It must have an at symbol (@) and a period (.) like this: abc@xyz.com");
+    });
+    test("returns empty string for a valid email", () => {
+        expect(validateEmail("anything@domain.org")).toEqual("");
+    });
+    const invalidEmailTestCases = [
+        { email: "anything.com", testCondition: "missing an @ symbol" },
+        { email: "someone@yahoo", testCondition: "missing a period" },
+        { email: "any@th@ing.com", testCondition: "has multiple @ symbols" },
+        { email: "someone@yahoo.com.org", testCondition: "has multiple periods" },
+        { email: "domain.com@henry", testCondition: "with @ symbol before period" },
+        { email: "@domain.com", testCondition: "with no name" },
+        { email: "bernice@.com", testCondition: "with no domain" },
+        { email: "sally@domain.", testCondition: "with no top-level domain" },
+    ];
+    invalidEmailTestCases.forEach(testCase => {
+        test(`returns error message for email address ${testCase.testCondition}`, () => {
+            const errorMessage = validateEmail(testCase.email);
+            expect(errorMessage).toEqual(INVALID_EMAIL_MESSAGE);
+        });
+    });
+});
---
+++ src/hooks/form-validation/validateEmail.ts
@@ -0,0 +1,26 @@
+import { Validator } from "./useValidations";
+
+export const INVALID_EMAIL_MESSAGE = "This is not a valid email address. It must have an at symbol (@) and a period (.) like this: abc@xyz.com";
+
+export const validateEmail: Validator = (emailAddress: string) => {
+    const atIndex = emailAddress.indexOf("@");
+    const atCount = (emailAddress.match(/@/g) || []).length;
+
+    const periodIndex = emailAddress.indexOf(".");
+    const periodCount = (emailAddress.match(/\./g) || []).length;
+
+    const invalid = atCount === 0
+        || periodCount === 0
+        || atCount > 1
+        || periodCount > 1
+        || periodIndex < atIndex
+        || atIndex === 0
+        || atIndex === periodIndex - 1
+        || periodIndex === emailAddress.length - 1;
+
+    if (invalid) {
+        return INVALID_EMAIL_MESSAGE;
+    }
+
+    return "";
+};
