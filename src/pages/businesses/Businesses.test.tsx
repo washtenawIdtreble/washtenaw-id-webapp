2db4c1ca06dacc2e942f716ce35bb0d58cdd8f76
Ryan Heisler <info@ryanheisler.com>
2022-12-03 17:36:42 -0500

    RHEI - fixed some non-compiling code. Only do one thing inside each waitfor

--- src/pages/businesses/Businesses.test.tsx
+++ src/pages/businesses/Businesses.test.tsx
@@ -1,35 +1,42 @@
 import React from "react";
-import {Businesses} from "./Businesses";
-import { render, screen, within, waitFor } from "@testing-library/react";
+import { Businesses } from "./Businesses";
+import { render, screen, waitFor, within } from "@testing-library/react";
 import { TEST_BUSINESSES } from "../../mock-server/businesses-resolver";
 import { MemoryRouter } from "react-router-dom";
 
 describe(Businesses.name, () => {
     beforeEach(() => {
-        render(<Businesses/>, { wrapper: MemoryRouter});
+        render(<Businesses/>, { wrapper: MemoryRouter });
     });
 
-    test("has a heading", ()=> {
+    test("has a heading", () => {
         const pageHeader = screen.getByRole("heading", { level: 1 });
         expect(pageHeader.textContent).toBe("Businesses that accept the ID");
     });
 
-    test("contains category name as heading for a set of businesses", async() => {
+    test("contains category name as heading for a set of businesses", async () => {
+        let categoryHeadings: HTMLHeadingElement[] = [];
         await waitFor(() => {
-            const categoryNames = screen.getAllByRole("heading", { level: 2 }).map(h2 => h2.textContent);
-            const titleCaseCategories = TEST_BUSINESSES.map(b => b.category.displayName);
-            expect(categoryNames).toEqual(titleCaseCategories);
+            categoryHeadings = screen.getAllByRole("heading", { level: 2 });
         });
-    });  
 
-    test("contains a list of businesses", async() => {
+        const categoryNames = categoryHeadings.map(h2 => h2.textContent);
+        const titleCaseCategories = TEST_BUSINESSES.map(b => b.category.displayName);
+        
+        expect(categoryNames).toEqual(titleCaseCategories);
+    });
+
+    test("contains a list of businesses", async () => {
+        let listElements: HTMLUListElement[] = [];
         await waitFor(() => {
-            const listElements = screen.getAllByRole("list");
-            let listItems : string[][]  = [];
-            listElements.map(listElement => { 
-                listItems.push(within(listElement).getAllByRole("listitem").map(li => li.textContent))
-            })
-            expect(listItems).toEqual(TEST_BUSINESSES.map(c => c.businesses));
+            listElements = screen.getAllByRole("list");
         });
-    });  
+
+        let businesses: string[][] = [];
+        listElements.forEach(listElement => {
+            let listItems = within(listElement).getAllByRole("listitem");
+            businesses.push(listItems.map(li => li.textContent ?? ""));
+        });
+        expect(businesses).toEqual(TEST_BUSINESSES.map(c => c.businesses));
+    });
 });  
--- src/pages/categories/Categories.test.tsx
+++ src/pages/categories/Categories.test.tsx
@@ -5,32 +5,34 @@ import { TEST_CATEGORIES } from "../../mock-server/resolvers/categories-resolver
 import { MemoryRouter } from "react-router-dom";
 import { PAGE_ENDPOINTS } from "../../layout/RouterOutlet";
 
-
 describe(Categories.name, () => {
     beforeEach(() => {
-        render(<Categories/>,  { wrapper: MemoryRouter });
+        render(<Categories/>, { wrapper: MemoryRouter });
     });
     test("contains a list of categories", async () => {
         const listElement = screen.getByRole("list");
-        let categoryNames : string[];
+        let categoryListItems: HTMLLIElement[] = [];
         await waitFor(() => {
-            categoryNames = within(listElement)
-                .getAllByRole("listitem")
-                .map(li => within(li).getByRole("link"))
-                .map(link => link.textContent);
+            categoryListItems = within(listElement).getAllByRole("listitem");
         });
+
+        const categoryNames = categoryListItems
+            .map(li => within(li).getByRole("link"))
+            .map(link => link.textContent ?? "");
         const categories = TEST_CATEGORIES.map(c => c.displayName);
+
         expect(categoryNames).toEqual(categories);
     });
 
-    test("links to correct url", async() => {
-        let categoryUrls : string[];
+    test("links to correct url", async () => {
+        let categoryLinks: HTMLAnchorElement[] = [];
         await waitFor(() => {
-            const categoryLinks : HTMLAnchorElement[] = screen.getAllByRole("link");
-            categoryUrls = categoryLinks.map(link => link.href);
+            categoryLinks = screen.getAllByRole("link");
         });
 
+        const categoryUrls = categoryLinks.map(link => link.href);
         const expectedCategoryUrls = TEST_CATEGORIES.map(c => `${PAGE_ENDPOINTS.businesses}#${c.category}`);
+
         expect(categoryUrls.length).toEqual(expectedCategoryUrls.length);
         expectedCategoryUrls.forEach((expectUrl, index) => {
             expect(categoryUrls[index].endsWith(expectUrl)).toBe(true);
