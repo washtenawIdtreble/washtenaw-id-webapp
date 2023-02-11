bb508c4996e4751dcddb02be463d087bd28690fc
Ryan Heisler <info@ryanheisler.com>
2023-02-11 12:36:28 -0500

    Treble 53 - Remove unused Alert Context and Providers component

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import "./App.css";
 import { Layout } from "./Layout";
 import { BrowserRouter } from "react-router-dom";
-import { Providers } from "./Providers";
 
 function App() {
     return (
-        <Providers>
-            <BrowserRouter>
-                <Layout/>
-            </BrowserRouter>
-        </Providers>
+        <BrowserRouter>
+            <Layout/>
+        </BrowserRouter>
     );
 }
 
