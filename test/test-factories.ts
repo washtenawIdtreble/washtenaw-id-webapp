12afea05c9037d7aae2b0bca07c9b363596bd101
Ryan Heisler <info@ryanheisler.com>
2023-07-04 13:39:45 -0400

    Remove unused test factories for alerts

--- test/test-factories.ts
+++ test/test-factories.ts
@@ -1,23 +1,7 @@
-import { AlertContextValue, AlertData } from "../src/contexts/AlertContext";
 import { AccessibilityFormData } from "../src/pages/accessibility-issues/AccessibilityIssues";
 import { Business, CategorizedBusinesses } from "../src/hooks/useBusinesses";
 import { faker } from "@faker-js/faker";
 import { Category } from "../src/hooks/useCategories";
-
-export const buildMockAlertContext = (attributes?: Partial<AlertContextValue>): AlertContextValue => {
-    attributes = attributes || {};
-    return {
-        showAlert: attributes.showAlert || jest.fn().mockName("mockShowAlert"),
-    };
-};
-
-export const stubAlertData = (attributes?: Partial<AlertData>): AlertData => {
-    attributes = attributes || {};
-    return {
-        heading: attributes.heading === undefined ? "Stub Alert Heading" : attributes.heading,
-        message: attributes.message === undefined ? "Stub Alert Message" : attributes.message,
-    };
-};
 
 export const stubAccessibilityFormData = (attributes: Partial<AccessibilityFormData> = {}): AccessibilityFormData => {
     return {
