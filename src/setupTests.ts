1e33c7a9887ac2443d288aceed0fa27e63a25a00
Ryan Heisler <info@ryanheisler.com>
2023-09-04 20:19:54 -0400

    Defer focusing a subheading until data is loaded
    pull all code to focus subheading into a function so it will be easier to defer it until loading is finished
    Pass loading boolean to hook that focuses hash or main heading so it can defer focusing until data is loaded
    Use environment variables for timeout and number of attempts to focus a subheading before falling back to the main heading.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -6,11 +6,14 @@ import "@testing-library/jest-dom";
 import "jest-when";
 import { setupTestingServer } from "./mock-server/setup-testing-server";
 import { toHaveNoViolations } from "jest-axe";
+import { ENVIRONMENT_VARIABLES } from "./utilities/environment-variables";
 
-process.env.REACT_APP_API = "test";
-process.env.REACT_APP_FOCUS_TIMEOUT = "10";
-process.env.REACT_APP_LOADING_TIMEOUT = "10";
-process.env.REACT_APP_LIVE_REGION_CLEAR_TIMEOUT = "100";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_API] = "test";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_FOCUS_TIMEOUT] = "10";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_LOADING_TIMEOUT] = "10";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_LIVE_REGION_CLEAR_TIMEOUT] = "100";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_SUBHEADING_FOCUS_TIMEOUT] = "5";
+process.env[ENVIRONMENT_VARIABLES.REACT_APP_SUBHEADING_FOCUS_ATTEMPTS] = "60";
 
 setupTestingServer();
 
