c1e68f119977381211efad75c8d86db1dd2f26d6
Ryan Heisler <info@ryanheisler.com>
2023-09-08 14:23:15 -0400

    Add box around text on ordinance page

--- src/pages/ann-arbor-ordinance/AnnArborOrdinance.tsx
+++ src/pages/ann-arbor-ordinance/AnnArborOrdinance.tsx
@@ -11,11 +11,12 @@ export const AnnArborOrdinance = () => {
     return (<>
         <MainHeading>{ORDINANCE_PAGE_HEADING}</MainHeading>
         <div className={"content-container"}>
-            <p>All businesses in Ann Arbor are required to accept the Washtenaw County ID as proof of identity.</p>
-            <p><OpensInANewTabLink
+            <p className={"ordinance-paragraph"}>All businesses in Ann Arbor are required to accept the Washtenaw County
+                ID as proof of identity.</p>
+            <p className={"ordinance-paragraph"}><OpensInANewTabLink
                 href={"https://library.municode.com/mi/ann_arbor/codes/code_of_ordinances?nodeId=TITIXPORE_CH112NSC_9_150IN"}>See
                 the law on Municode</OpensInANewTabLink></p>
-            <p>
+            <p className={"ordinance-paragraph"}>
                 If a business in Ann Arbor refused to accept your Washtenaw County ID,&nbsp;
                 <AppLink to={PAGE_ENDPOINTS.contactUs}>
                     please tell us about your experience
