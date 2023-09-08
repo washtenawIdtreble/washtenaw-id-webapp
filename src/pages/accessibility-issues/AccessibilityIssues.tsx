3ed433da7ccdcbba8f62f5f2d48df244cded1434
Ryan Heisler <info@ryanheisler.com>
2023-09-07 21:17:26 -0400

    Move form fields inside labels and fix textarea size styling on mobile and desktop views

--- src/pages/accessibility-issues/AccessibilityIssues.tsx
+++ src/pages/accessibility-issues/AccessibilityIssues.tsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
-import '../Pages.css';
-import { Form } from '../../components/form/Form';
-import { SERVER_ENDPOINTS } from '../../utilities/server-endpoints';
-import { FormField, FormFieldType } from '../../components/form/FormField';
-import { validateEmail } from '../../hooks/form-validation/validateEmail';
-import { validatePhone } from '../../hooks/form-validation/validatePhone';
-import { validateRequired } from '../../hooks/form-validation/validateRequired';
-import { MainHeading } from '../../components/MainHeading';
-import { MAIN_HEADING_ID } from '../../components/MainHeading';
+import React, { useState } from "react";
+import "../Pages.css";
+import { Form } from "../../components/form/Form";
+import { SERVER_ENDPOINTS } from "../../utilities/server-endpoints";
+import { FormField, FormFieldType } from "../../components/form/FormField";
+import { validateEmail } from "../../hooks/form-validation/validateEmail";
+import { validatePhone } from "../../hooks/form-validation/validatePhone";
+import { validateRequired } from "../../hooks/form-validation/validateRequired";
+import { MAIN_HEADING_ID, MainHeading } from "../../components/MainHeading";
 
 export type AccessibilityFormData = {
     name?: string;
@@ -16,12 +15,12 @@ export type AccessibilityFormData = {
     description: string;
 };
 
-export const ACCESSIBILITY_PAGE_HEADING = 'Report Accessibility Issues';
-export const ACCESSIBILITY_PAGE_IDENTIFIER = 'accessibility-issues';
+export const ACCESSIBILITY_PAGE_HEADING = "Report Accessibility Issues";
+export const ACCESSIBILITY_PAGE_IDENTIFIER = "accessibility-issues";
 
 export const AccessibilityIssues = () => {
     const [successMessage] = useState(
-        'Your issue has been reported, thank you!'
+        "Your issue has been reported, thank you!"
     );
     return (
         <>
@@ -32,43 +31,43 @@ export const AccessibilityIssues = () => {
                 ariaLabelledBy={MAIN_HEADING_ID}
                 submitEndpoint={SERVER_ENDPOINTS.ACCESSIBILITY_ISSUES}
             >
-                <label htmlFor={'name'} className={'form-label'}>
+                <label htmlFor={"name"} className={"form-label"}>
                     Your Name (optional)
+                    <FormField id={"name"}
+                               pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
+                               autoComplete={"name"}
+                               name={"name"}/>
                 </label>
-                <FormField id={'name'} 
-                    pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER} 
-                    autoComplete={'name'} 
-                    name={'name'} />
-                <label htmlFor={'email'} className={'form-label'}>
+                <label htmlFor={"email"} className={"form-label"}>
                     Your email (optional)
+                    <FormField
+                        id={"email"}
+                        pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
+                        name={"email"}
+                        validator={validateEmail}
+                        autoComplete={"email"}
+                    />
                 </label>
-                <FormField
-                    id={'email'}
-                    pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
-                    name={'email'}
-                    validator={validateEmail}
-                    autoComplete={'email'}
-                />
-                <label htmlFor={'phone'} className={'form-label'}>
+                <label htmlFor={"phone"} className={"form-label"}>
                     Your phone number (optional)
+                    <FormField
+                        id={"phone"}
+                        pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
+                        name={"phone"}
+                        validator={validatePhone}
+                        autoComplete={"tel"}
+                    />
                 </label>
-                <FormField
-                    id={'phone'}
-                    pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
-                    name={'phone'}
-                    validator={validatePhone}
-                    autoComplete={'tel'}
-                />
-                <label htmlFor={'description'} className={'form-label'}>
+                <label htmlFor={"description"} className={"form-label textarea-label"}>
                     What do you want to tell us? (required)
+                    <FormField
+                        id={"description"}
+                        pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
+                        name={"description"}
+                        validator={validateRequired}
+                        inputType={FormFieldType.TEXTAREA}
+                    />
                 </label>
-                <FormField
-                    id={'description'}
-                    pageIdentifier={ACCESSIBILITY_PAGE_IDENTIFIER}
-                    name={'description'}
-                    validator={validateRequired}
-                    inputType={FormFieldType.TEXTAREA}
-                />
             </Form>
         </>
     );
