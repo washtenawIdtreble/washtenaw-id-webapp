55c485fa5be2eec19c3d1d9ef3fc44096f24de4e
Ryan Heisler <info@ryanheisler.com>
2023-11-02 14:10:03 -0400

    Remove routes and links to the businesses and categories pages ahead of the first launch because we don't have data to put on them.

--- src/layout/RouterOutlet.tsx
+++ src/layout/RouterOutlet.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Categories, CATEGORIES_PAGE_HEADING } from "../pages/categories/Categories";
-import { Businesses, BUSINESSES_PAGE_HEADING } from "../pages/businesses/Businesses";
 import { ACCESSIBILITY_PAGE_HEADING, AccessibilityIssues } from "../pages/accessibility-issues/AccessibilityIssues";
 import { CONTACT_PAGE_HEADING, ContactUs } from "../pages/contact-us/ContactUs";
 import { Page } from "../pages/Page";
@@ -22,12 +20,6 @@ export const RouterOutlet = () => {
                 <Route path={PAGE_ENDPOINTS.annArborOrdinance}
                        element={<Page title={ORDINANCE_PAGE_HEADING}
                                       key={ORDINANCE_PAGE_HEADING}><AnnArborOrdinance/></Page>}/>
-                <Route path={PAGE_ENDPOINTS.categories}
-                       element={<Page title={CATEGORIES_PAGE_HEADING}
-                                      key={CATEGORIES_PAGE_HEADING}><Categories/></Page>}/>
-                <Route path={PAGE_ENDPOINTS.businesses}
-                       element={<Page title={BUSINESSES_PAGE_HEADING}
-                                      key={BUSINESSES_PAGE_HEADING}><Businesses/></Page>}/>
                 <Route path={PAGE_ENDPOINTS.accessibilityIssues}
                        element={<Page title={ACCESSIBILITY_PAGE_HEADING}
                                       key={ACCESSIBILITY_PAGE_HEADING}><AccessibilityIssues/></Page>}/>
