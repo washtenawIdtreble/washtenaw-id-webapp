15166f63a1245c313ac1752b692342bea9f7609f
Ryan Heisler <info@ryanheisler.com>
2023-11-29 20:25:16 -0500

    Style both text pages and fix small issues

--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import { MainHeading } from "../../components/MainHeading";
 import { AppLink } from "../../components/navigation/AppLink";
 import { PAGE_ENDPOINTS } from "../../layout/RouterOutlet";
-import "./WelcomePage.css";
 
 export const WELCOME_PAGE_HEADING = "Washtenaw ID in Ann Arbor";
 
 export const WelcomePage = () => {
     return (<>
         <MainHeading>{WELCOME_PAGE_HEADING}</MainHeading>
-        <div className={"welcome-page-container"}>
-            <p className={"welcome-page-paragraph"}>All businesses in Ann Arbor are required to accept the Washtenaw
+        <div className={"text-page-container"}>
+            <p className={"text-page-paragraph"}>All businesses in Ann Arbor are required to accept the Washtenaw
                 County
                 ID as proof of identity.</p>
-            <p className={"welcome-page-paragraph"}>
+            <p className={"text-page-paragraph"}>
                 <AppLink className={"inline-link"} to={PAGE_ENDPOINTS.annArborLaw}>
-                    Learn more about Ann Arbor's law
+                    Click here to learn more about Ann Arbor's law
                 </AppLink>
             </p>
-            <p className={"welcome-page-paragraph"}>
+            <p className={"text-page-paragraph"}>
                 If a business in Ann Arbor refused to accept your Washtenaw County ID, please tell us about your
                 experience.&nbsp;
                 <AppLink to={PAGE_ENDPOINTS.reportIdRefused} className={"inline-link"}>
