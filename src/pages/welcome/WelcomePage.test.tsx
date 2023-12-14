5ae97b70a2417bde18a0c661d87acf9f57a4c064
Ryan Heisler <info@ryanheisler.com>
2023-12-13 20:53:18 -0500

    Add welcome video and transcript to welcome page
    remove autoplay from allowed features of youtube video
    remove related videos from the end of the youtube video

--- src/pages/welcome/WelcomePage.test.tsx
+++ src/pages/welcome/WelcomePage.test.tsx
@@ -28,25 +28,67 @@ describe(WelcomePage.name, () => {
         expect(h1.hasAttribute("tabindex")).toBe(true);
         expect(h1.tabIndex).toBe(-1);
     });
-    test("has information about the law in Ann Arbor", () => {
-        const info = screen.getByText("All businesses in Ann Arbor are required to accept the Washtenaw County ID as proof of identity.");
-        expect(info).toBeVisible();
-    });
-    test("has a link to a summary of Ann Arbor's law", () => {
-        const link: HTMLAnchorElement = screen.getByRole("link", { name: "Click here to learn more about Ann Arbor's law" });
-        expect(link).toBeVisible();
-        expect(link.href.endsWith(PAGE_ENDPOINTS.annArborLaw)).toBe(true);
-    });
-    test("has a call-to-action about businesses that don't accept the ID", () => {
-        const nonBreakingSpace = `\xa0`;
-        const paragraphText = `If a business in Ann Arbor refused to accept your Washtenaw County ID, please tell us about your experience.${nonBreakingSpace}`;
-        const paragraph = screen.getByText((text, element) => {
-            return element?.textContent?.startsWith(paragraphText) ?? false;
-        });
-        expect(paragraph).toBeVisible();
 
-        const link: HTMLAnchorElement = within(paragraph).getByRole("link", { name: "Click here to contact us." });
-        expect(link).toBeVisible();
-        expect(link.href).toContain(PAGE_ENDPOINTS.reportIdRefused);
+    describe("information section", () => {
+        let informationContainer: HTMLDivElement;
+        beforeEach(() => {
+            informationContainer = screen.getByTestId("information-container");
+        });
+        test("has information about the law in Ann Arbor", () => {
+            const info = within(informationContainer).getByText("All businesses in Ann Arbor are required to accept the Washtenaw County ID as proof of identity.");
+            expect(info).toBeVisible();
+        });
+        test("has a link to a summary of Ann Arbor's law", () => {
+            const link: HTMLAnchorElement = within(informationContainer).getByRole("link", { name: "Click here to learn more about Ann Arbor's law" });
+            expect(link).toBeVisible();
+            expect(link.href.endsWith(PAGE_ENDPOINTS.annArborLaw)).toBe(true);
+        });
+        test("has a call-to-action about businesses that don't accept the ID", () => {
+            const nonBreakingSpace = `\xa0`;
+            const paragraphText = `If a business in Ann Arbor refused to accept your Washtenaw County ID, please tell us about your experience.${nonBreakingSpace}`;
+            const paragraph = within(informationContainer).getByText((_text, element) => {
+                return element?.textContent?.startsWith(paragraphText) ?? false;
+            });
+            expect(paragraph).toBeVisible();
+
+            const link: HTMLAnchorElement = within(paragraph).getByRole("link", { name: "Click here to contact us." });
+            expect(link).toBeVisible();
+            expect(link.href).toContain(PAGE_ENDPOINTS.reportIdRefused);
+        });
     });
+
+    test("has a heading demarcating the video section", () => {
+        expect(screen.getByRole("heading", { level: 2, name: "Washtenaw ID Project Info Video" })).toBeInTheDocument();
+    });
+
+    test("has a video", () => {
+        let videoIFrame: HTMLIFrameElement = screen.getByTitle("Washtenaw County ID Welcome Video");
+
+        expect(videoIFrame).toBeVisible();
+        expect(videoIFrame.src).toEqual("https://www.youtube-nocookie.com/embed/d76T3dwcm3M?si=4yTPVyjYmkAhYC7W&rel=0");
+    });
+
+    describe("transcript", () => {
+        let transcriptContainer: HTMLDivElement;
+        beforeEach(() => {
+            transcriptContainer = screen.getByTestId("transcript-container");
+        });
+        test("has a heading", () => {
+            expect(within(transcriptContainer).getByRole("heading", {
+                level: 3,
+                name: "Video Transcript"
+            })).toBeInTheDocument();
+        });
+        test("has a transcript of the video", () => {
+            expect(within(transcriptContainer).getByText("The video above contains interviews with people involved in the Washtenaw ID Project. While each person is talking, we see them speaking directly to the camera.")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Keta Cowan - Co-Founder and Chair, The Washtenaw ID Project: \"40,000 individuals both citizens and undocumented folks lack an ID card in Washtenaw County. The Washtenaw ID Project is a local initiative to provide a government-issued ID card to those who cannot access a state ID.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Jerry L. Clayton - Sheriff, Washtenaw County: \"My grandparents came up to live with my parents from the south. They didn't have a birth certificate. They didn't have a Social Security card.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Yousef Rabhi - Michigan State Representative, 53rd District: \"You don't have those privileges. You don't have those advantages.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Janelle Zini - Co-Founder, Washtenaw ID Project: \"Think about where you get asked for your ID - it's almost daily.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Sheriff Clayton: \"Not being able to get a bank account, not being able to um sometimes secure uh a house or an apartment or a job.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Laura Radzik - AVP, Old National Bank: \"Old National accepts the Washtenaw ID as a primary form of identification meaning it works the same as if you had a driver's license or a passport.\"")).toBeVisible();
+            expect(within(transcriptContainer).getByText("Rep. Rabhi: \"This ID card is really in my opinion the most American thing that we could do. It is the most in line with our values of who we are as a country.\"")).toBeVisible();
+        });
+    });
+
 });
--- src/pages/welcome/WelcomePage.tsx
+++ src/pages/welcome/WelcomePage.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { MainHeading } from "../../components/MainHeading";
 import { AppLink } from "../../components/navigation/AppLink";
 import { PAGE_ENDPOINTS } from "../../layout/RouterOutlet";
+import "./WelcomePage.css";
 
 export const WELCOME_PAGE_HEADING = "Washtenaw ID in Ann Arbor";
 
 export const WelcomePage = () => {
     return (<>
         <MainHeading>{WELCOME_PAGE_HEADING}</MainHeading>
-        <div className={"text-page-container"}>
+        <div className={"text-page-container"} data-testid={"information-container"}>
             <p className={"text-page-paragraph"}>All businesses in Ann Arbor are required to accept the Washtenaw
                 County
                 ID as proof of identity.</p>
@@ -24,6 +25,39 @@ export const WelcomePage = () => {
                     Click here to contact us.
                 </AppLink>
             </p>
+        </div>
+        <h2 className={"visually-hidden"}>Washtenaw ID Project Info Video</h2>
+        <div className={"video-container"}>
+            <iframe
+                src="https://www.youtube-nocookie.com/embed/d76T3dwcm3M?si=4yTPVyjYmkAhYC7W&rel=0"
+                title="Washtenaw County ID Welcome Video" frameBorder="0"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowFullScreen
+                className={"video-iframe"}
+            />
+        </div>
+        <div className={"text-page-container transcript-container"} data-testid={"transcript-container"}>
+            <h3 className={"transcript-heading"}>Video Transcript</h3>
+            <p className={"text-page-paragraph"}>The video above contains interviews with people involved in the
+                Washtenaw ID Project. While each person is talking, we see them speaking directly to the camera.</p>
+            <p className={"text-page-paragraph"}>Keta Cowan - Co-Founder and Chair, The Washtenaw ID Project: "40,000
+                individuals both citizens and undocumented folks lack an ID card in Washtenaw County. The Washtenaw ID
+                Project is a local initiative to provide a government-issued ID card to those who cannot access a state
+                ID."</p>
+            <p className={"text-page-paragraph"}>Jerry L. Clayton - Sheriff, Washtenaw County: "My grandparents came up
+                to live with my parents from the south. They didn't have a birth certificate. They didn't have a Social
+                Security card."</p>
+            <p className={"text-page-paragraph"}>Yousef Rabhi - Michigan State Representative, 53rd District: "You
+                don't have those privileges. You don't have those advantages."</p>
+            <p className={"text-page-paragraph"}>Janelle Zini - Co-Founder, Washtenaw ID Project: "Think about where
+                you get asked for your ID - it's almost daily."</p>
+            <p className={"text-page-paragraph"}>Sheriff Clayton: "Not being able to get a bank account, not being able
+                to um sometimes secure uh a house or an apartment or a job."</p>
+            <p className={"text-page-paragraph"}>Laura Radzik - AVP, Old National Bank: "Old National accepts the
+                Washtenaw ID as a primary form of identification meaning it works the same as if you had a driver's
+                license or a passport."</p>
+            <p className={"text-page-paragraph"}>Rep. Rabhi: "This ID card is really in my opinion the most American
+                thing that we could do. It is the most in line with our values of who we are as a country."</p>
         </div>
     </>);
 };
